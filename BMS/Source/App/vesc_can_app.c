4427064793cfb918e7fc38aa62085adb1e8c4a78
Michał Modzelewski <michal.modzelewski@gmail.com>
2025-07-04 14:07:08 +0200

    send all can status packets

--- BMS/Source/App/vesc_can_app.c
+++ BMS/Source/App/vesc_can_app.c
@@ -1,4 +1,5 @@
 #include "vesc_can_app.h"
+#include "vesc_can.h"
 
 /**************************************************
  * @brie  :VESC_CAN_Status_Task()
@@ -27,16 +28,20 @@ void VESC_CAN_Status_Task(void)
 	Software_Counter_1ms.VESC_CAN = 0;
 
 	VESC_Set_BMS_V_TOT(&VESC_CAN_DATA);
+	VESC_Set_BMS_I(&VESC_CAN_DATA);
+	VESC_Set_BMS_AH_WH(&VESC_CAN_DATA);
 	for(i=0;i<MAX_CELL_SERIES;i+=3)
 	{
 		VESC_Set_BMS_V_CELL(&VESC_CAN_DATA, i);
 	}
 	VESC_Set_BMS_BAL(&VESC_CAN_DATA);
-	VESC_Set_BMS_I(&VESC_CAN_DATA);
-	VESC_Set_BMS_HUM(&VESC_CAN_DATA);
 	for(i=0;i<MAX_TEMP_SENSORS;i+=3)
 	{
 		VESC_Set_BMS_TEMPS(&VESC_CAN_DATA,i);
 	}
+	VESC_Set_BMS_HUM(&VESC_CAN_DATA);
+	VESC_Set_BMS_SOC_SOH_TEMP_STAT(&VESC_CAN_DATA);
+	VESC_Set_BMS_AH_WH_CHG_TOTAL(&VESC_CAN_DATA);
+	VESC_Set_BMS_AH_WH_DIS_TOTAL(&VESC_CAN_DATA);
 }
 
