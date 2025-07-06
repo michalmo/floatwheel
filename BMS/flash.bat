637525b9da106baccdd18ef7aee84c7df151d235
Michał Modzelewski <michal.modzelewski@gmail.com>
2025-07-07 00:59:08 +0200

    update flash scripts to use uv to manage python and pyocd

--- BMS/flash.bat
+++ BMS/flash.bat
@@ -7,65 +7,55 @@ if "%~1"=="" (
     set "firmware_file=%~1"
 )
 
-REM Check firmware file exists
+REM Check that the firmware file exists
 if not exist "%firmware_file%" (
-    echo Firmware file not found: %firmware_file%.
-    echo.
-    echo Make sure you have copied your preferred lcm hex file into this folder and have named it "lcm.hex"
+    echo File not found: %firmware_file%
     pause
-    exit 1
+    exit /b 1
 )
 
-REM Check Python is installed
-python --version >nul 2>nul
+REM Get the extension of the firmware file
+for %%i in ("%firmware_file%") do set "extension=%%~xi"
+set "extension=%extension:~1%"
+
+REM Verify that the firmware file is a .hex
+if /i not "%extension%"=="hex" (
+    echo File is not a .hex file
+    pause
+    exit /b 1
+)
+
+REM Check that the uv command is installed
+uvx --version >nul 2>nul
 if errorlevel 1 (
-    echo Python is not installed or was not found in your PATH
-    echo.
-    echo Install Python 3.7 using this link: https://www.python.org/ftp/python/3.7.9/python-3.7.9-amd64.exe
-    echo.
-    echo === IMPORTANT ===
-    echo Choose "Custom",
-    echo Accept all defaults on page 1, click Next
-    echo On page 2, check the checkbox "add to environment"
-    echo no other changes required
+    echo uv is not installed or was not found in your PATH
+    echo Install it with: winget install --id=astral-sh.uv -e
+    echo or: powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+    echo or choose one of the alternative installation methods documented at https://docs.astral.sh/uv/getting-started/installation/
     pause
-    exit 1
+    exit /b 1
 )
 
-REM Check pyocd is installed
-pyocd --version >nul 2>nul
-if errorlevel 1 (
-    echo pyocd is not installed or was not found in your PATH
-    echo.
-    echo Open CMD.exe, then run this command:
-    echo python -m pip install --upgrade pyocd==0.34.3
-    echo If you have installed pyocd but still see this error, please add it to your PATH
-    pause
-    exit 1
-)
-
-REM Check HKMicroChip.HK32F030xMxx_DFP.1.0.17.pack exists
+REM Check that NSING.N32L40x_DFP.1.0.1.pack exists
 if not exist "NSING.N32L40x_DFP.1.0.1.pack" (
     echo NSING.N32L40x_DFP.1.0.1.pack not found
-    echo.
-    echo Make sure you have extracted all the files into a folder
+    echo Download it from https://github.com/michalmo/floatwheel/raw/refs/heads/bms-vesc/BMS/NSING.N32L40x_DFP.1.0.1.pack
     pause
-    exit 1
+    exit /b 1
 )
 
-echo Before continuing, make sure you have installed the ST-Link drivers from:
-echo https://www.st.com/en/development-tools/stsw-link004.html
-echo.
-pause
-echo.
-REM Erase and flash the firmware
-echo Erasing the old firmware...
-pyocd erase -c -t N32L403K8 --pack NSING.N32L40x_DFP.1.0.1.pack
-echo.
+REM Delimiter to separate the output
+set "delimiter========"
+
+REM Flash the firmware
+echo %delimiter%
 echo Flashing the new firmware...
-pyocd load "%firmware_file%" -t N32L403K8 --pack NSING.N32L40x_DFP.1.0.1.pack
-echo.
-echo Please check the flashing messages to make sure the flashing was successful.
+echo %delimiter%
+uvx pyocd@0.35.1 load "%firmware_file%" -t N32L403KB --pack NSING.N32L40x_DFP.1.0.1.pack
+
+echo %delimiter%
+echo Please check the flashing messages to make sure the flash was successful.
 echo Float on!
-echo.
+echo %delimiter%
+
 pause
--- BMS/flash.sh
+++ BMS/flash.sh
@@ -25,33 +25,29 @@ if [ "$extension" != "hex" ]; then
     exit 1
 fi
 
-# Check that the pyocd command is installed
-if [ ! -x "$(command -v pyocd)" ]; then
-    echo "pyocd is not installed or was not found in your PATH"
-    echo "Install it with: python -m pip install --upgrade pyocd==0.34.3"
-    echo "If you have installed pyocd and it cannot be found, please add it to your PATH"
+# Check that the uvx command is installed
+if [ ! -x "$(command -v uvx)" ]; then
+    echo "uv is not installed or was not found in your PATH"
+    echo "Install it with: curl -LsSf https://astral.sh/uv/install.sh | sh"
+    echo "or choose one of the alternative installation methods documented at https://docs.astral.sh/uv/getting-started/installation/"
     exit 1
 fi
 
 # Check that NSING.N32L40x_DFP.1.0.1.pack exists
 if [ ! -f "./NSING.N32L40x_DFP.1.0.1.pack" ]; then
     echo "NSING.N32L40x_DFP.1.0.1.pack not found"
-    echo "Download it from https://github.com/surfdado/floatwheel"
+    echo "Download it from https://github.com/michalmo/floatwheel/raw/refs/heads/bms-vesc/BMS/NSING.N32L40x_DFP.1.0.1.pack"
     exit 1
 fi
 
 # Delimiter to separate the output
 delimiter="========"
 
-# Erase and flash the firmware
-echo "Erasing the old firmware..."
-echo "$delimiter"
-pyocd erase -c -t N32L403K8 --pack NSING.N32L40x_DFP.1.0.1.pack
-
+# Flash the firmware
 echo "$delimiter"
 echo "Flashing the new firmware..."
 echo "$delimiter"
-pyocd load "$firmware_file" -t N32L403K8 --pack NSING.N32L40x_DFP.1.0.1.pack
+uvx pyocd@0.35.1 load "$firmware_file" -t N32L403KB --pack NSING.N32L40x_DFP.1.0.1.pack
 
 echo "$delimiter"
 echo "Please check the flashing messages to make sure the flash was successful."
